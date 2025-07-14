45d314b91f3a6c8095186894b2ef7548cf1d3492
S-A-oghene <agbamogunjacob@gmail.com>
2025-07-14 17:47:50 +0100

    Added route handler to debug rendering issues test 1

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+// Added this root route handler to debug rendering issues
+app.get("/", (req, res) => {
+  res.send("Welcome to the CSE341 API!");
+});
+
 app.use("/", require("./routes"));
 
 mongodb.initDb((err, mongodb) => {
