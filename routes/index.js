39967f63d46edc6e7ecfb7420babaf290e5536f7
S-A-oghene <agbamogunjacob@gmail.com>
2025-07-14 17:02:02 +0100

    working API routes week 1

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-router.get("/", (req, res) => {
-  res.send("Hello World! Welcome to the home page!");
-});
+// router.get("/", (req, res) => {
+//   res.send("Hello World! Welcome to the home page!");
+// });
+router.use("/contacts", require("./contacts"));
 module.exports = router;
